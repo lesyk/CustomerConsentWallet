8a670cd43ea1c09d8b7b6cd82237ae5d584858ca
Linas Zvirblis <lzv@trifork.com>
2016-12-15 15:11:34 +0100

    Extended Whisper form for easy communication with the Identity Service.

--- app/controllers/whisper.js
+++ app/controllers/whisper.js
@@ -6,9 +6,43 @@ export default Ember.Controller.extend({
 
   listening: false,
   topic: null,
+  topics: new Map(),
+  topicNames: [],
   payload: null,
   messages: [],
   filter: null,
+  idServiceAddress: null,
+
+  listenForIdService: function () {
+
+    let self = this;
+    let web3 = this.get('web3.web3Instance');
+
+    web3.shh.filter({
+      topics: [web3.fromAscii('identity-service-advertisement')]
+    }).watch(function (err, result) {
+
+      let idServiceAdvertisement = JSON.parse(web3.toAscii(result.payload));
+
+      idServiceAdvertisement.topics.forEach(function (topic) {
+
+        let topics = self.get("topics");
+        topics.set(topic.topic, topic);
+
+        self.get("topicNames").clear();
+        topics.forEach(function (topic) {
+          self.get("topicNames").pushObject(topic.topic);
+        });
+
+        if (!self.get('topic')) {
+          self.set('topic', self.get("topicNames.0"));
+        }
+      });
+
+      console.log("Identity service at address " + result.from, err, result);
+      self.set("idServiceAddress", result.from);
+    });
+  },
 
   listen: function () {
 
@@ -36,6 +70,7 @@ export default Ember.Controller.extend({
 
       var message = {
         from: identity,
+        to: this.get('idServiceAddress'),
         topics: [web3.fromAscii(topic)],
         payload: web3.fromAscii(payload),
         ttl: 1000,
@@ -45,6 +80,11 @@ export default Ember.Controller.extend({
       web3.shh.post(message, function (err, result) {
         console.log("Message post", err, result);
       });
+    },
+
+    topicChanged: function (topic) {
+      this.set('topic', topic);
+      this.set('payload', JSON.stringify(this.get('topics').get(topic).request));
     }
   }
 });
--- app/routes/whisper.js
+++ app/routes/whisper.js
@@ -1,3 +1,8 @@
 import Ember from "ember";
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+
+  setupController: function () {
+    this.controller.listenForIdService();
+  }
+});
