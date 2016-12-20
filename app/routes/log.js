aa8e07da8c749fb3968031ad48e57303c717e5c8
Viktor Lesyk <vee.lesyk@gmail.com>
2016-12-20 15:16:20 +0100

    Remove: unused

--- app/routes/log.js
+++ app/routes/log.js
@@ -8,20 +8,6 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   totalCount: 0,
   log: [],
 
-  model() {
-    console.log('model')
-    let web3 = this.get("web3").instance();
-
-    return RSVP.hash({
-      count: web3.eth.getTransactionCount("0x04979CbC97b1902d3E3B57960D40B1fa90a99730"),
-      lastBlockNumber: web3.eth.blockNumber,
-      lastBlock: web3.eth.getBlock(9596),
-      transaction: web3.eth.getTransaction("0xb2899de4e855d25c5592125d066ee1610a82ec8b983e2689b1f14788086a1d7b"),
-      log: [],
-      totalCount: 0
-    });
-  },
-
   setupController: function(controller, model) {
     this._super(controller, model);
     console.log('setupController')
@@ -29,17 +15,6 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     let web3 = this.get("web3").instance();
     this.set('log', []);
     this.set('totalCount', 0);
-
-    //show all transacitons
-    // let countAllTransactions = 0
-    // for(let i = 0; i < web3.eth.blockNumber; i++) {
-    //     let block = web3.eth.getBlock(i);
-    //     if(block.transactions.length != 0) {
-    //       console.log(block.transactions);
-    //       countAllTransactions += 1;
-    //     }
-    // }
-    // console.log(countAllTransactions);
 
     //show all transacitons except: from account -> ember generated;
     let filter = web3.eth.filter({fromBlock:0, toBlock: 'pending'});
