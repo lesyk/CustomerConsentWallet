a267724240f35577d2db92120dcc5ae51e709dd8
Viktor Lesyk <vee.lesyk@gmail.com>
2017-01-05 10:51:29 +0100

    Add: deploying of a contract

--- dataowner/index.js
+++ dataowner/index.js
@@ -1,20 +1,39 @@
 "use strict";
 
-let Web3 = require('web3');
-let ConsentLib = require('consentlib');
+const Web3 = require('web3');
+const solc = require('solc')
+const ConsentLib = require('consentlib');
+const fs = require("fs");
 
 let web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://node0:8545'));
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 let email = process.argv[2];
-
 let whisper = new ConsentLib.Whisper(web3);
 let consentFlow = new ConsentLib.ConsentFlow(whisper);
-
 let identity = whisper.newIdentity();
 let ttl = 100;
 let priority = 100;
 
 consentFlow.discoverIdentityService((err, result, idServiceAddress) => {
     setTimeout(() => consentFlow.registerWhisperId(identity, idServiceAddress, email, ttl, priority), 200);
+});
+
+let account = web3.personal.unlockAccount(web3.eth.accounts[0], 'password');
+let source = fs.readFileSync('./../contracts/PayingBackContract.sol', 'utf8');
+let compiledContract = solc.compile(source, 1);
+let abi = compiledContract.contracts['PayingBackContract'].interface;
+console.log('Abi', abi);
+let contract = web3.eth.contract(JSON.parse(abi));
+let faucetInstanceTransaction = contract.new({from:web3.eth.accounts[0], data:compiledContract.contracts['PayingBackContract'].bytecode, value:30000000000000000000, gas:1000000}, function(err, myContract){
+  if(!err) {
+     if(!myContract.address) {
+         console.log("Hash: ", myContract.transactionHash);
+     } else {
+         console.log("Address: ", myContract.address);
+     }
+  }
+  else {
+    console.log(err);
+  }
 });
