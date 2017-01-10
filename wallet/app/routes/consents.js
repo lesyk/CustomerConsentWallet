777f323be8ce49f25b3261ecbb7c95067058c492
Linas Zvirblis <lzv@trifork.com>
2017-01-10 14:18:57 +0100

    Register the user in the Identity Service and respond to Ethereum address requests.

--- wallet/app/routes/consents.js
+++ wallet/app/routes/consents.js
@@ -1,82 +1,93 @@
-import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import Consent from '../models/consent';
-import ethJSABI from "npm:ethereumjs-abi";
+import Ember from "ember";
+import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
+import Consent from "../models/consent";
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   web3: Ember.inject.service(),
+  consentLib: Ember.inject.service("consentlib"),
   contractService: Ember.inject.service('consent-contract'),
+  session: Ember.inject.service('session'),
   contract: null,
   contractEvents: [],
   consents: [],
 
-  setupController: function(controller, model) {
-    let web3 = this.get("web3").instance();
-    let addresses = web3.currentProvider.transaction_signer.getAddresses();
-    web3.eth.defaultAccount = addresses[0];
+  setupController: function (controller, model) {
 
+    let web3 = this.get("web3").instance();
+    let consentLib = this.get("consentLib").initialize(web3);
+    let email = this.get("session").get("data.email");
+    let addresses = web3.currentProvider.transaction_signer.getAddresses();
+
+    web3.eth.defaultAccount = addresses[0];
+    consentLib.registerWithIdService(email);
+    consentLib.respondEthAddress(addresses[0]);
+
+    controller.set("email", email);
     this.set("contract", this.get("contractService").getConsentContract());
     this.fetchConsents(this.get("contract"), "0x" + addresses[0]);
   },
 
-  fetchConsents: function(contract, customer_address) {
+  fetchConsents: function (contract, customer_address) {
     let web3 = this.get("web3").instance();
 
     let consentResultCallback = (error, result) => {
-        if(!error) {
-          let consent = Consent.create({
-            requester: result[0],
-            customer: result[1],
-            owner: result[2],
-            state: result[3].toString(10),
-            id: result[4]
-          });
-          this.get("consents").push(consent);
-          this.get("controller").set("consents", this.get("consents"));
-        } else {
-          console.log(error);
-        }
+      if (!error) {
+        let consent = Consent.create({
+          requester: result[0],
+          customer: result[1],
+          owner: result[2],
+          state: result[3].toString(10),
+          id: result[4]
+        });
+        this.get("consents").push(consent);
+        this.get("controller").set("consents", this.get("consents"));
+      } else {
+        console.log(error);
+      }
     };
 
     contract.customerConsents(customer_address, (error, result) => {
-        if(!error) {
-          let length = result.toString(10);
-          for(var i = 0; i < length; i++) {
-              contract.customer_mapping(customer_address, i , (error, result) => {
-                if(!error) {
-                  contract.getConsent(result.toString(10), consentResultCallback);
-                } else {
-                  console.log(error);
-                }
-              });
-          }
-        } else {
-          console.log(error);
+      if (!error) {
+        let length = result.toString(10);
+        for (var i = 0; i < length; i++) {
+          contract.customer_mapping(customer_address, i, (error, result) => {
+            if (!error) {
+              contract.getConsent(result.toString(10), consentResultCallback);
+            } else {
+              console.log(error);
+            }
+          });
         }
+      } else {
+        console.log(error);
+      }
     });
   },
 
   actions: {
-    accept: function(consent, i) {
+    accept: function (consent, i) {
       let web3 = this.get("web3").instance();
       let contract = this.get("contract");
       let controller = this.get("controller");
       let gasPrice = 50000000000;
       let gas = 500000;
 
-      contract.giveConsent(web3.toHex(consent.requester), web3.toHex(consent.owner), web3.toHex(consent.id), {gas: gas, gasPrice: gasPrice}, function(error, result) {
-        if(error !== null) {
+      contract.giveConsent(web3.toHex(consent.requester), web3.toHex(consent.owner), web3.toHex(consent.id), {
+        gas: gas,
+        gasPrice: gasPrice
+      }, function (error, result) {
+        if (error !== null) {
           console.log(error);
           controller.set("eventError", "Request could not be completed");
         } else {
 
-          let givenEvent = contract.ConsentGiven(function(error, result) {
-            if(!error) {
+          let givenEvent = contract.ConsentGiven(function (error, result) {
+            if (!error) {
               console.log(result.args);
 
               // TODO update UI
               // TODO possibly show feedback on when transaction is mined (& has x confirmations)
-              if(result.args.given === true) {
+              if (result.args.given === true) {
                 consent.state = 1;
               }
             } else {
@@ -86,7 +97,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
         }
       });
     },
-    reject: function(id) {
+    reject: function (id) {
       console.log("reject");
     }
   }
---
+++ wallet/app/services/consentlib.js
@@ -0,0 +1,43 @@
+import Ember from "ember";
+import ConsentLib from "npm:consentlib";
+const {Service} = Ember;
+
+export default Service.extend({
+
+  web3: null,
+  whisper: null,
+  consentFlow: null,
+  identity: null,
+  address: null,
+
+  ttl: 100,
+  priority: 100,
+
+  initialize: function (web3) {
+
+    if (!this.get("web3")) {
+      let whisper = new ConsentLib.Whisper(web3);
+      let consentFlow = new ConsentLib.ConsentFlow(web3, whisper, null);
+      let identity = whisper.newIdentity();
+
+      this.set("web3", web3);
+      this.set("whisper", whisper);
+      this.set("consentFlow", consentFlow);
+      this.set("identity", identity);
+    }
+
+    return this;
+  },
+
+  registerWithIdService: function (email) {
+    this.set("email", email);
+    this.get("consentFlow").discoverIdentityService((err, result, idServiceAddress) => {
+      setTimeout(() => this.get("consentFlow").registerWhisperId(this.get("identity"), idServiceAddress, email, this.get("ttl"), this.get("priority")), 200);
+    });
+  },
+
+  respondEthAddress: function (address) {
+    this.set("address", address);
+    this.get("consentFlow").respondEthAddress(this.get("identity"), this.get("email"), address, this.get("ttl"), this.get("priority"));
+  }
+});
