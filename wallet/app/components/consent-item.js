8a0e297c71c6237d89290181d2321d26da5dbaea
Rikke Willer <rwi@trifork.com>
2017-01-11 13:07:32 +0100

    Update UI when customer give consent action is done

--- wallet/app/components/consent-item.js
+++ wallet/app/components/consent-item.js
@@ -3,33 +3,38 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   web3: Ember.inject.service(),
   contractService: Ember.inject.service('consent-contract'),
+  isDone: false,
+
+  rejected: function(errorMsg) {
+    console.error(errorMsg);
+    alert("Updating consent failed: " + errorMsg);
+  },
 
   actions: {
-    accept: function(consent) {
+    acceptConsent: function(consent) {
       let web3 = this.get("web3").instance();
       let contract = this.get("contractService").getConsentContract();
       let gasPrice = 50000000000;
       let gas = 500000;
 
       return new Ember.RSVP.Promise((resolve, reject) => {
-
         contract.giveConsent(web3.toHex(consent.requester), web3.toHex(consent.owner), web3.toHex(consent.id), {gas: gas, gasPrice: gasPrice}, (error, result) => {
-          if(error !== null) {
-            reject(error);
+          if (error !== null) {
+            this.rejected(error);
+            reject();
           } else {
-            let givenEvent = contract.ConsentGiven(function(error, result) {
-              if(!error) {
-                console.log(result.args);
-
-                // TODO update UI
-                if(result.args.given === true) {
-                  consent.state = 1;
+            let givenEvent = contract.ConsentGiven((error, result) => {
+              if (!error) {
+                if (result.args.id === consent.id && result.args.given === true) {
+                  this.$(".state").text("GIVEN");
+                  this.set("isDone", true);
                   resolve();
                 } else {
+                  this.rejected("Consent could not be given according to contract specification");
                   reject();
                 }
               } else {
-                console.log(error);
+                this.rejected(error);
                 reject();
               }
             });
@@ -37,9 +42,8 @@ export default Ember.Component.extend({
         });
       });
     },
-    reject: function(id) {
+    rejectConsent: function(id) {
       console.log("reject");
     }
   }
-
 });
