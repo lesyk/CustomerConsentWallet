c13c3242a6a603857ead9c4c78a4ec696b78f392
Rikke Willer <rwi@trifork.com>
2017-01-12 11:02:26 +0100

    Temporary workaround to issue with production build

--- wallet/ember-cli-build.js
+++ wallet/ember-cli-build.js
@@ -4,7 +4,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    minifyJS: {
+      options: {
+        exclude: ["**/vendor.js"]
+      }
+    }
   });
 
   app.import('bower_components/web3/dist/web3.min.js');
