12e32b3bb387471c486ca1bfcb3ab0f57f1fba58
Viktor Lesyk <vee.lesyk@gmail.com>
2017-04-20 14:34:12 +0200

    Add: log of events

--- dataowner/src/ApplyForConcentForm.js
+++ dataowner/src/ApplyForConcentForm.js
@@ -11,6 +11,8 @@ class ApplyForConcentForm extends React.Component {
       dataOwnerEmail: "datao@example.com",
       password: "password",
       address: "0x9a804CF13E3defb1043D2B81BE95D14C787c94Dc",
+      finished: false,
+      messages: "Form submitted.",
     };
   }
 
@@ -27,6 +29,8 @@ class ApplyForConcentForm extends React.Component {
   makeConsentRequest = (e) => {
     e.preventDefault();
 
+    this.setState({submitted: true});
+
     let web3 = new Web3();
     web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
@@ -41,41 +45,65 @@ class ApplyForConcentForm extends React.Component {
     web3.eth.defaultAccount = this.state.address;
 
     consentFlow.discoverIdentityService((err, result, idServiceAddress) => {
-        setTimeout(() => consentFlow.registerWhisperId(identity, idServiceAddress, this.state.dataOwnerEmail, ttl, priority), 200);
+        setTimeout(() => {
+          consentFlow.registerWhisperId(identity, idServiceAddress, this.state.dataOwnerEmail, ttl, priority)
+        }, 200);
     });
 
     consentFlow.respondEthAddress(identity, this.state.dataOwnerEmail, this.state.address, ttl, priority);
 
-    setTimeout(() => consentFlow.dataRequested().then((result) => consentFlow.provideData(result.args.customer, result.args.data_requester, result.args.id.toString(10), "http://url.to.the.customer.data/data1.xml")), 1000);
+    setTimeout(() => consentFlow.dataRequested().then((result) => {
+      consentFlow.provideData(result.args.customer, result.args.data_requester, result.args.id.toString(10), "http://url.to.the.customer.data/data1.xml").then((result) => {
+        this.setState(prevState => ({ messages: prevState.messages + `\nData provided at transaction ${result}`, finished: true }));
+      }).catch((error) => {
+        this.setState(prevState => ({ messages: prevState.messages + `\nFailed to provide data${error}`, finished: true }));
+      })
+    }), 1000);
   }
 
   render() {  
     return (
-      <form className="form-horizontal">
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Data Owner Email</label>
-          <div className="col-sm-10">
-            <input className="form-control" type="text" value={this.state.dataOwnerEmail} onChange={this.handleChange} />
+      <div>
+      {this.state.submitted ? (
+          <div className="panel panel-default">
+            <div className="panel-heading">Log</div>
+            <div className="panel-body">
+              {this.state.messages.split('\n').map((item, key) => {
+                return <span key={key}>{item}<br/></span>
+              })}
+              {this.state.finished == false &&
+                (<span>Mining...</span>)
+              }
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Password</label>
-          <div className="col-sm-10">
-            <input className="form-control" type="text" value={this.state.password} onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Address</label>
-          <div className="col-sm-10">
-            <input className="form-control" type="text" value={this.state.address} onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="col-sm-offset-2 col-sm-10">
-            <button className="btn btn-default" onClick={this.makeConsentRequest}>Apply for Consent</button>
-          </div>
-        </div>
-      </form>
+        ) : (
+          <form className="form-horizontal">
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Data Owner Email</label>
+              <div className="col-sm-10">
+                <input className="form-control" type="text" value={this.state.dataOwnerEmail} onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Password</label>
+              <div className="col-sm-10">
+                <input className="form-control" type="text" value={this.state.password} onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Address</label>
+              <div className="col-sm-10">
+                <input className="form-control" type="text" value={this.state.address} onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-offset-2 col-sm-10">
+                <button className="btn btn-default" onClick={this.makeConsentRequest}>Apply for Consent</button>
+              </div>
+            </div>
+          </form>
+        )}
+      </div>
     );
   }
 }
--- datarequester/src/ApplyForConcentForm.js
+++ datarequester/src/ApplyForConcentForm.js
@@ -12,7 +12,10 @@ class ApplyForConcentForm extends React.Component {
       password: "password",
       address: "0xA04A6A76F1E23D80E075f490a2E23daACB65d0A9",
       customerEmail: "person@example.com",
-      ownerEmail: "datao@example.com"
+      ownerEmail: "datao@example.com",
+      submitted: false,
+      finished: false,
+      messages: "Form submitted.",
     };
   }
 
@@ -29,6 +32,8 @@ class ApplyForConcentForm extends React.Component {
   makeConsentRequest = (e) => {
     e.preventDefault();
 
+    this.setState({submitted: true});
+
     let web3 = new Web3();
     web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
@@ -44,59 +49,91 @@ class ApplyForConcentForm extends React.Component {
     web3.eth.defaultAccount = this.state.address;
 
     consentFlow.discoverIdentityService((err, result, idServiceAddress) => {
-        setTimeout(() => consentFlow.registerWhisperId(identity, idServiceAddress, this.state.dataRequestorEmail, ttl, priority), 200);
+        setTimeout(() => {
+          consentFlow.registerWhisperId(identity, idServiceAddress, this.state.dataRequestorEmail, ttl, priority)
+        }, 200);
         setTimeout(() => {
           consentFlow.lookupWhisperIds(identity, idServiceAddress, [this.state.ownerEmail, this.state.customerEmail], ttl, priority, 9000).then((whisperIds) =>
             consentFlow.lookupEthAddresses(identity, whisperIds, ttl, priority, 9000)
           ).then((ethAddresses) =>
-            consentFlow.requestConsent(ethAddresses.get(this.state.customerEmail), ethAddresses.get(this.state.ownerEmail), consentId)
+            consentFlow.requestConsent(ethAddresses.get(this.state.customerEmail), ethAddresses.get(this.state.ownerEmail), consentId).then((result)=> {
+              this.setState(prevState => ({ messages: prevState.messages + `\nConsent requested at transaction ${result}` }));
+            }).catch((error) => {
+              this.setState(prevState => ({ messages: prevState.messages + `\nFailed to request a consent ${error}`, finished: true }));
+            })
           );
         }, 1000);
     });
 
-    setTimeout(() => consentFlow.consentGiven(consentId).then((result) => consentFlow.requestData(result.args.customer, result.args.data_owner, result.args.id.toString(10))), 1000);
-    setTimeout(() => consentFlow.dataProvided(consentId).then((result) => console.log(result)), 1000);    
+    setTimeout(() => {
+      consentFlow.consentGiven(consentId).then((result) => {
+        consentFlow.requestData(result.args.customer, result.args.data_owner, result.args.id.toString(10))
+      })
+    }, 1000);
+    setTimeout(() => {
+      consentFlow.dataProvided(consentId).then((result) => {
+        this.setState(prevState => ({ messages: prevState.messages + `\nData provided ${result}`, finished: true }));
+      }).catch((error) => {
+        this.setState(prevState => ({ messages: prevState.messages + `\nFailed read data provided ${error}`, finished: true }));
+      })
+    }, 1000);
   }
 
   render() {  
     return (
-      <form className="form-horizontal">
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Data Requestor Email</label>
-          <div className="col-sm-10">
-            <input className="form-control" type="text" value={this.state.dataRequestorEmail} onChange={this.handleChange} />
+      <div>
+        {this.state.submitted ? (
+          <div className="panel panel-default">
+            <div className="panel-heading">Log</div>
+            <div className="panel-body">
+              {this.state.messages.split('\n').map((item, key) => {
+                return <span key={key}>{item}<br/></span>
+              })}
+              {this.state.finished == false &&
+                (<span>Mining...</span>)
+              }
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Password</label>
-          <div className="col-sm-10">
-            <input className="form-control"  type="text" value={this.state.password} onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Address</label>
-          <div className="col-sm-10">
-            <input className="form-control"  type="text" value={this.state.address} onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Data Owner Email</label>
-          <div className="col-sm-10">
-            <input className="form-control"  type="text" value={this.state.ownerEmail} onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Customer's Email</label>
-          <div className="col-sm-10">
-            <input className="form-control"  type="text" value={this.state.customerEmail} onChange={this.handleChange} />
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="col-sm-offset-2 col-sm-10">
-            <button className="btn btn-primary" onClick={this.makeConsentRequest}>Apply for Consent</button>
-          </div>
-        </div>
-      </form>
+        ) : (
+          <form className="form-horizontal">
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Data Requestor Email</label>
+              <div className="col-sm-10">
+                <input className="form-control" type="text" value={this.state.dataRequestorEmail} onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Password</label>
+              <div className="col-sm-10">
+                <input className="form-control"  type="text" value={this.state.password} onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Address</label>
+              <div className="col-sm-10">
+                <input className="form-control"  type="text" value={this.state.address} onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Data Owner Email</label>
+              <div className="col-sm-10">
+                <input className="form-control"  type="text" value={this.state.ownerEmail} onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-sm-2 control-label">Customer's Email</label>
+              <div className="col-sm-10">
+                <input className="form-control"  type="text" value={this.state.customerEmail} onChange={this.handleChange} />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-offset-2 col-sm-10">
+                <button className="btn btn-primary" onClick={this.makeConsentRequest}>Apply for Consent</button>
+              </div>
+            </div>
+          </form>
+        )}
+      </div>
     );
   }
 }
